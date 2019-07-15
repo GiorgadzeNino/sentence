db6e675fe1c92fee9933c318e934f097ec75cf39
nino-coder <nino.giorgadze.2000@gmail.com>
2019-07-15 17:23:49 +0400

    edited

--- sentence.js
+++ sentence.js
@@ -1,16 +1,21 @@
 let str="windows is bad, is not it?. Linux is good."
-let words=str.split(" ")
+let string=str.replace(/[.,\!^*/;:{}=-?_%&`~()]/g,"")
+let words=string.split(" ")
+
+
 function counting(word)
 {
-    for (let i = 0; i < word.length; i++) {
- 
-		let count = 0;
-		for (let w = 0; w < word.length; w++) {
-			if (word[i] == word[w]) {
+	let set=new Set()
+	  for (let i = 0; i < word.length; i++) {
+		 let count = 0;
+		for (let w = 0; w <word.length; w++) {
+			if (word[i] === word[w]) {
 				count++;
-			}
-        }
-       console.log(word[i], count)
-}  
+			}			
+				}
+				set.add(words[i]+":"+count)			
+}
+console.log(set)
 }
 counting(words)
+
